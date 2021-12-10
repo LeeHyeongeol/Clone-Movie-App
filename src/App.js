eefc249d05e6634e985696a3eb48e8e8125ddda8
hyeongirlife <leehyeongul@gmail.com>
2021-12-10 09:46:01 +0900

    컴포넌트 재사용성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 // import Potato from './potato'; //  ./ 는 현재파일이 있는 폴더라는 뜻이다.
 
-function Potato() {
+function Food(props) {
+  console.log(props)
+  const { fav } = props
   return (
     <div>
-      <h3>안녕하세요. 말하는 감자입니다.</h3>
+      {/* props.fav로 입력해도 된다. */}
+      <h3>안녕하세요. 말하는 {fav} 입니다.</h3>
     </div>
   )
 }
@@ -15,7 +18,9 @@ function App() {
     <div>
       <h1>"이현걸입니다."</h1>
       <h2>토스뱅크 입사를 희망합니다.</h2>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   )
 }
